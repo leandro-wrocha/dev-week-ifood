e2d36acaeda037923b43b32c96fe4af44a540487
Leandro Rocha <leandro.wrocha98@gmail.com>
2023-06-29 13:24:23 -0300

    add examples of possibles code with same result

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ data = pd.read_csv('./contents/feedbacks.csv', delimiter=';')
 
 feedbacks = [Feedback(line['grade'], line['comment']) for index, line in data.iterrows()]
 
+# this same result of code above
+# feedbacks = dados.apply(lambda linha: Feedback(linha['nota'], linha['comentario']), axis=1)
+
 report = ReportFeedback(feedbacks)
 nps = report.calculate_nps()
 
--- services/reportFeedback.py
+++ services/reportFeedback.py
@@ -6,4 +6,9 @@ class ReportFeedback:
     detractors = sum([1 for feedback in self.feedbacks if feedback.grade <= 6])
     prosecutors = sum([1 for feedback in self.feedbacks if feedback.grade >= 9])
 
+    # functiona lambda is a fuction anonymous, only to filter
+    # this example more simple to return the same value of variables above
+    # detratores = notas.apply(lambda nota: nota <= 6).sum()
+    # promotores = notas[notas >= 9].count()
+
     return (prosecutors - detractors) / len(self.feedbacks) * 100
